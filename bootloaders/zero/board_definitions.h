593cab3108f0568e624b4e0e7120734d72fe8210
Rachel Berkhahn <tasogarenokagi@gmail.com>
2016-12-29 21:59:59 -0500

    Built and flashed bootloader to featherlight rev C prototypes.

--- bootloaders/zero/board_definitions.h
+++ bootloaders/zero/board_definitions.h
@@ -27,6 +27,8 @@
   #include "board_definitions_genuino_mkr1000.h"
 #elif defined(BOARD_ID_arduino_mkrzero)
   #include "board_definitions_arduino_mkrzero.h"
+#elif defined(BOARD_ID_featherlight)
+  #include "board_definitions_featherlight.h"
 #else
   #error You must define a BOARD_ID and add the corresponding definitions in board_definitions.h
 #endif
